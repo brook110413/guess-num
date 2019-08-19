638d23306c4f584b9c6b4ccad005943e80b9ff27
Jason Hung <brook110413@gmail.com>
2019-08-19 22:14:02 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('請輸入數字範圍最小值')
+end = input('請輸入數字範圍最大值')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
@@ -14,5 +18,3 @@ while True:
 	elif r < num:
 		print('比你的數字小: ')
 	print('這是你猜的第', count, '次')
-
-
